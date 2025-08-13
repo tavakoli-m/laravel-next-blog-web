a25a6cca22af321a95a7393cffc279b15d33d3ab
Mohammadreza Tavakoli <heyreza8@gmail.com>
2025-08-14 00:13:20 +0330

    add react query

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,26 @@
 import '@/public/css/bootstrap.min.css'
 import '@/public/css/style.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
