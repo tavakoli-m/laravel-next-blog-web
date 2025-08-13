326d258fb95ea25a334ab5e12b149e207935e89d
Mohammadreza Tavakoli <heyreza8@gmail.com>
2025-08-14 02:52:45 +0330

    refactor login and register feature

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,50 +1,53 @@
 import { Http } from "@/services/Http";
 import { useMutation } from "@tanstack/react-query";
+import { useFormik } from "formik";
 import Cookies from "js-cookie";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import * as Yup from 'yup'
 
 const Login = () => {
-
-    const [inputs, setInputs] = useState({})
     const router = useRouter()
-
-    const handleChnage = (event) => {
-        setInputs({ ...inputs, [event.target.name]: event.target.value })
-    }
-
-    const { mutate } = useMutation({
+    const { mutate: loginApi } = useMutation({
         mutationFn: (values) => Http.post('/v1/login', values),
         onSuccess: ({ data: { data } }) => {
             Cookies.set('auth_token', data.token, { expires: 30, path: '/' })
             router.push('/')
         }
     })
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        mutate(inputs)
-    }
+    const form = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        onSubmit: (data) => loginApi(data),
+        validationSchema: Yup.object({
+            email: Yup.string().required().email().max(255),
+            password: Yup.string().required().min(8),
+        })
+    })
 
     return (
-        <>
             <section id="app">
-
                 <section style={{ "height": "100vh", backgroundColor: "#138496" }}
                     className="d-flex justify-content-center align-items-center">
                     <section style={{ "width": "20rem" }}>
                         <h1 className="bg-warning rounded-top px-2 mb-0 py-3 h5">Laravel + Next Blog login</h1>
-                        <section className="bg-light my-0 px-2"><small className="text-danger"></small></section>
-                        <form className="pt-3 pb-1 px-2 bg-light rounded-bottom" onSubmit={handleSubmit}>
+                        <form className="pt-3 pb-1 px-2 bg-light rounded-bottom" onSubmit={form.handleSubmit}>
                             <section className="form-group">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" onChange={handleChnage} name="email" id="email" placeholder="email ..." />
+                                <input type="email" className="form-control"{...form.getFieldProps('email')} id="email" placeholder="email ..." />
+                                {form.errors.email && form.touched.email && (
+                                    <span className="text-danger">{form.errors.email}</span>
+                                )}
                             </section>
                             <section className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" onChange={handleChnage} name="password" id="password"
+                                <input type="password" className="form-control" {...form.getFieldProps('password')} id="password"
                                     placeholder="password ..." />
+                                {form.errors.email && form.touched.email && (
+                                    <span className="text-danger">{form.errors.email}</span>
+                                )}
                             </section>
                             <section className="mt-4 mb-2 d-flex justify-content-between">
                                 <input type="submit" className="btn btn-success btn-sm" value="login" />
@@ -54,7 +57,6 @@ const Login = () => {
                     </section>
                 </section>
             </section>
-        </>
     )
 }
 
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,57 +1,69 @@
 import { Http } from "@/services/Http";
 import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
-import { useState } from "react";
 import Cookies from 'js-cookie';
 import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from 'yup'
 
 const Register = () => {
-    const [inputs, setInputs] = useState({})
     const router = useRouter()
-
-    const handleChnage = (event) => {
-        setInputs({ ...inputs, [event.target.name]: event.target.value })
-    }
-
-    const { mutate } = useMutation({
+    const { mutate: registerApi } = useMutation({
         mutationFn: (values) => Http.post('/v1/register', values),
         onSuccess: ({ data: { data } }) => {
             Cookies.set('auth_token', data.token, { expires: 30, path: '/' })
             router.push('/')
         }
     })
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (inputs.password === inputs.confirm) {
-            mutate(inputs)
-        }
-    }
+    const form = useFormik({
+        initialValues: {
+            email: "",
+            username: "",
+            password: "",
+            confirm: ""
+        },
+        onSubmit: (data) => registerApi(data),
+        validationSchema: Yup.object({
+            email: Yup.string().required().email().max(255),
+            username: Yup.string().required().max(255),
+            password: Yup.string().required().min(8),
+            confirm: Yup.string().required().oneOf([Yup.ref('password')], 'Confirm Password Not Acceptable')
+        })
+    })
 
     return (
-        <>
             <section id="app">
-
                 <section style={{ "height": "100vh", backgroundColor: "#138496" }} className="d-flex justify-content-center align-items-center">
                     <section style={{ "width": "20rem" }}>
                         <h1 className="bg-warning rounded-top px-2 mb-0 py-3 h5">Laravel + Next Blog Register</h1>
-                        <section className="bg-light my-0 px-2"><small className="text-danger"></small></section>
-                        <form className="pt-3 pb-1 px-2 bg-light rounded-bottom" onSubmit={handleSubmit}>
+                        <form className="pt-3 pb-1 px-2 bg-light rounded-bottom" onSubmit={form.handleSubmit}>
                             <section className="form-group">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" name="email" id="email" onChange={handleChnage} placeholder="email ..." />
+                                <input type="email" className="form-control" id="email" {...form.getFieldProps('email')} placeholder="email ..." />
+                                {form.errors.email && form.touched.email && (
+                                    <span className="text-danger">{form.errors.email}</span>
+                                )}
                             </section>
                             <section className="form-group">
                                 <label htmlFor="username">User Name</label>
-                                <input type="text" className="form-control" name="username" id="username" onChange={handleChnage} placeholder="username ..." />
+                                <input type="text" className="form-control" id="username" {...form.getFieldProps('username')} placeholder="username ..." />
+                                {form.errors.username && form.touched.username && (
+                                    <span className="text-danger">{form.errors.username}</span>
+                                )}
                             </section>
                             <section className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" name="password" id="password" onChange={handleChnage} placeholder="password ..." />
+                                <input type="password" className="form-control" id="password" {...form.getFieldProps('password')} placeholder="password ..." />
+                                {form.errors.password && form.touched.password && (
+                                    <span className="text-danger">{form.errors.password}</span>
+                                )}
                             </section>
                             <section className="form-group">
                                 <label htmlFor="confirm">Confirm</label>
-                                <input type="password" className="form-control" name="confirm" id="confirm" onChange={handleChnage} placeholder="confirm ..." />
+                                <input type="password" className="form-control" id="confirm" {...form.getFieldProps('confirm')} placeholder="confirm ..." />
+                                {form.errors.confirm && form.touched.confirm && (
+                                    <span className="text-danger">{form.errors.confirm}</span>
+                                )}
                             </section>
                             <section className="mt-4 mb-2 d-flex justify-content-between">
                                 <input type="submit" className="btn btn-success btn-sm" value="register" />
@@ -60,9 +72,7 @@ const Register = () => {
                         </form>
                     </section>
                 </section>
-
             </section>
-        </>
     )
 }
 
