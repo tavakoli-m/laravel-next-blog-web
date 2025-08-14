f3e1f1f49783c12168247926cd4d4e19a73a9df2
Mohammadreza Tavakoli <heyreza8@gmail.com>
2025-08-14 22:41:00 +0330

    add check auth for admin panel

--- src/layouts/admin/Layout.jsx
+++ src/layouts/admin/Layout.jsx
@@ -1,7 +1,19 @@
+import { useAuth } from "@/context/AuthContext";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+    const { data, isLoading } = useAuth()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (data === undefined && isLoading === false) {
+            router.push('/')
+        }
+    }, [data, isLoading])
+
     return (
         <>
             <section id="app">
